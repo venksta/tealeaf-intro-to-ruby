d81264453cf90b0dd0ecb902abfd8a59ebc309ce
Alberto De Lucca <albertodelucca@gmail.com>
2013-03-08 02:36:54 -0430

    created oo blackjack file

---
+++ hw9_blackjack.rb
@@ -0,0 +1,5 @@
+# Blackjack Game
+# Author: Alberto De Lucca
+# Object-Oriented exercise for Tealeaf's Academy Intro to Ruby course.
+# A rewrite of:
+# github.com/adeluccar/tealeaf-intro-to-ruby/blob/master/hw6_blackjack.rb
