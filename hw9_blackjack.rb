1d8988f21e1ab87a3a32a31a989e45e27a995325
Alberto De Lucca <albertodelucca@gmail.com>
2013-03-11 16:07:41 -0430

    defined Card class

--- hw9_blackjack.rb
+++ hw9_blackjack.rb
@@ -5,94 +5,126 @@
 # github.com/adeluccar/tealeaf-intro-to-ruby/blob/master/hw6_blackjack.rb
 
 class Card
-  # must have one suit
-  # must have one rank
-  # must have a description string e.g. "Ace of Spades"
+
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def short_description
+    "#{rank.upcase}#{suit.upcase}"
+  end
+
+  def long_description
+    long_rank = case rank
+                when "A" then "Ace"
+                when "J" then "Jack"
+                when "Q" then "Queen"
+                when "K" then "King"
+                else rank
+                end
+
+    long_suit = case suit
+                when "H" then "Hearts"
+                when "S" then "Spades"
+                when "C" then "Clubs"
+                when "D" then "Diamonds"
+                end
+
+    "#{long_rank} of #{long_suit}"
+
+  end
 end
 
-class Deck
-  # if deck is recreated after every turn, then one deck suffices.
-  # must have 52 cards.
-  # cannot have duplicates.
+c = Card.new("5", "C")
+p c.short_description
+p c.long_description
 
-  # POSSIBLE METHODS
+# class Deck
+#   # if deck is recreated after every turn, then one deck suffices.
+#   # must have 52 cards.
+#   # cannot have duplicates.
 
-  # build a deck of 52 cards
-  # shuffle deck
-  # deal_a_card from Deck to Hand
-end
+#   # POSSIBLE METHODS
 
-class Hand
-  # inherit from Deck? true: A hand is a small deck attached to a player.
-  # if hand value > 21 player loses
+#   # build a deck of 52 cards
+#   # shuffle deck
+#   # deal_a_card from Deck to Hand
+# end
 
-  # a hand belongs to a player or dealer
+# class Hand
+#   # inherit from Deck? true: A hand is a small deck attached to a player.
+#   # if hand value > 21 player loses
 
-  ### POSSIBLE METHODS ###
+#   # a hand belongs to a player or dealer
 
-  # compute value of hand
-  # print a hand
-  # split the hand
-end
+#   ### POSSIBLE METHODS ###
 
-class Player
-  # must have name
-  # must have purse
-  # must bet in order to play a hand
+#   # compute value of hand
+#   # print a hand
+#   # split the hand
+# end
 
-  # can have multiple hands (after a split for example).
-  # can "hit" (add a card to hand)
-  # can "stand" (leave hand as is)
-  # can "surrender" (take hand out of play and receive half of bet back)
-  # can "double down" (double bet and receive ONLY one more card)
-  # can "split" (split hand and play two hands. only if bet is matched)
+# class Player
+#   # must have name
+#   # must have purse
+#   # must bet in order to play a hand
 
-  ### POSSIBLE METHODS ###
+#   # can have multiple hands (after a split for example).
+#   # can "hit" (add a card to hand)
+#   # can "stand" (leave hand as is)
+#   # can "surrender" (take hand out of play and receive half of bet back)
+#   # can "double down" (double bet and receive ONLY one more card)
+#   # can "split" (split hand and play two hands. only if bet is matched)
 
-  # get name
-  # get purse
-  # want to rebuy chips?
-  # place a bet
-end
+#   ### POSSIBLE METHODS ###
 
-class Dealer
-  # inherit from Player? true: A dealer is still a player.
+#   # get name
+#   # get purse
+#   # want to rebuy chips?
+#   # place a bet
+# end
 
-  # cannot have multiple hands.
-  # does not need name.
-  # can log bets won
-end
+# class Dealer
+#   # inherit from Player? true: A dealer is still a player.
 
-class Purse
-  # attached to a player
-  # must be > 0 for player to be in game.
-  # can be replenished
-end
+#   # cannot have multiple hands.
+#   # does not need name.
+#   # can log bets won
+# end
 
-class Bet
-  # inherit from Purse? A bet is a sub-purse attached to a player & hand.
-  # must be < purse to be valid (players cannot bet what they don't have)
-  # a bet belongs to a player
-  # a dealer doesn't bet
-end
+# class Purse
+#   # attached to a player
+#   # must be > 0 for player to be in game.
+#   # can be replenished
+# end
 
-class Round
-  # one round includes a betting & playing turn of players & a turn of dealer.
+# class Bet
+#   # inherit from Purse? A bet is a sub-purse attached to a player & hand.
+#   # must be < purse to be valid (players cannot bet what they don't have)
+#   # a bet belongs to a player
+#   # a dealer doesn't bet
+# end
 
-  # round starts
-  # a deck is created
-  # one card is dealt and shown to every player
-  # one card is dealt to the dealer but not shown
-  # one card is dealt and shown to every player
-  # one card is dealt and shown to the dealer
-  # each player hits, stays, splits, surrenders or doubles down
-  # each hand value is computed until player either stays or busts
-  # if player busts, he's out of the round
-  # when all players end their plays, dealer reveals hidden card
-  # if dealer's vale is < 17 dealer hits
-  # if dealer's value is >= 17 dealer stays
-  # if player's value is > dealer player wins
-  # if player's value is < dealer player loses
-  # if player's value == dealer's they tie
-  # round ends
-end
+# class Round
+#   # one round includes a betting & playing turn of players & a turn of dealer.
+
+#   # round starts
+#   # a deck is created
+#   # one card is dealt and shown to every player
+#   # one card is dealt to the dealer but not shown
+#   # one card is dealt and shown to every player
+#   # one card is dealt and shown to the dealer
+#   # each player hits, stays, splits, surrenders or doubles down
+#   # each hand value is computed until player either stays or busts
+#   # if player busts, he's out of the round
+#   # when all players end their plays, dealer reveals hidden card
+#   # if dealer's vale is < 17 dealer hits
+#   # if dealer's value is >= 17 dealer stays
+#   # if player's value is > dealer player wins
+#   # if player's value is < dealer player loses
+#   # if player's value == dealer's they tie
+#   # round ends
+# end
